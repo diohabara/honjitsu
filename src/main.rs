732d03470d6878825c2f8df9e78a2d1b420673c5
tuampon <diohabara@gmail.com>
2022-11-21 17:55:45 -0600

    feat: comply with tweet restriction

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,69 @@ use chrono_tz::America::Chicago;
 use chrono_tz::Tz;
 use honjitsu::todoist::get_todoist_completed_tasks;
 use honjitsu::toggl::get_entry_project_to_duration;
+
 use honjitsu::twitter::create_tweet;
 use log::info;
 
-async fn get_toggle_text_for_tweet(date: Date<Tz>) -> Result<String, reqwest::Error> {
-    let mut tweet_contents: Vec<String> = Vec::new();
-    tweet_contents.push(format!("{}", date.format("%Y/%m/%d")));
+async fn get_toggle_text_for_tweet(date: Date<Tz>) -> Result<Vec<String>, reqwest::Error> {
+    let mut toggl_tweets: Vec<String> = Vec::new();
     let toggl_pairs = get_entry_project_to_duration(date).await?;
     let mut previous_project = "".to_string();
-    for ((project, task), duration) in toggl_pairs.iter() {
+    let mut current_tasks = Vec::new();
+    current_tasks.push(format!("Toggl {}", date.format("%Y/%m/%d")));
+    for (i, ((project, task), duration)) in toggl_pairs.iter().enumerate() {
+        if current_tasks.is_empty() {
+            current_tasks.push(format!("Toggl {}", date.format("%Y/%m/%d")));
+        }
         if &previous_project != project {
-            tweet_contents.push(format!("*{project}"));
+            current_tasks.push(format!("⏰{project}"));
             previous_project = project.to_string();
         }
-        tweet_contents.push(format!(
+        current_tasks.push(format!(
             "{task}={:02}:{:02}",
             duration.num_minutes() / 60,
             duration.num_minutes() % 60
         ));
+        // emoji(2char) + project + \n + task + =hh:mm(6char)
+        if i + 1 < toggl_pairs.len()
+            && 240
+                < current_tasks.len()
+                    + toggl_pairs[i + 1].0 .1.len()
+                    + 1
+                    + toggl_pairs[i + 1].0 .0.len()
+                    + 6
+                    + 2
+        {
+            toggl_tweets.push(current_tasks.join("\n"));
+            current_tasks = Vec::new();
+        }
     }
-    Ok(tweet_contents.join("\n"))
+    if !current_tasks.is_empty() {
+        toggl_tweets.push(current_tasks.join("\n"));
+    }
+    Ok(toggl_tweets)
 }
 
-async fn get_todoist_text_for_tweet(date: Date<Tz>) -> Result<String, reqwest::Error> {
-    let mut todoist_text = Vec::new();
-    todoist_text.push(format!("Tasks {}", date.format("%Y/%m/%d")));
-    todoist_text.append(&mut get_todoist_completed_tasks(date).await?);
-    Ok(todoist_text.join("\n"))
+async fn get_todoist_text_for_tweet(date: Date<Tz>) -> Result<Vec<String>, reqwest::Error> {
+    let mut todoist_tweets = Vec::new();
+    let tasks = get_todoist_completed_tasks(date).await?;
+    let mut current_tasks = Vec::new();
+    current_tasks.push(format!("Todoist {}", date.format("%Y/%m/%d")));
+    for i in 0..tasks.len() {
+        if current_tasks.is_empty() {
+            current_tasks.push(format!("Todoist {}", date.format("%Y/%m/%d")));
+        }
+        current_tasks.push(format!("✅{}", tasks[i]));
+        // emoji(2 characters) + task
+        if i + 1 < tasks.len() && 240 < current_tasks.len() + tasks[i + 1].len() + 2 {
+            todoist_tweets.push(current_tasks.join("\n"));
+            current_tasks = Vec::new();
+        }
+    }
+    if !current_tasks.is_empty() {
+        todoist_tweets.push(current_tasks.join("\n"));
+    }
+    Ok(todoist_tweets)
 }
 
 #[tokio::main]
@@ -40,11 +76,15 @@ async fn main() -> Result<(), reqwest::Error> {
     let utc = Utc::now().naive_utc();
     let today = Chicago.from_utc_datetime(&utc).date();
     let yesterday = today - Duration::days(1);
-    let todoist_text_for_tweet = get_todoist_text_for_tweet(yesterday).await?;
-    let toggl_text_for_tweet = get_toggle_text_for_tweet(yesterday).await?;
-    info!("{todoist_text_for_tweet} {}", todoist_text_for_tweet.len());
-    info!("{toggl_text_for_tweet} {}", toggl_text_for_tweet.len());
-    create_tweet(todoist_text_for_tweet.as_str()).await;
-    create_tweet(toggl_text_for_tweet.as_str()).await;
+    let todoist_text_for_tweets = get_todoist_text_for_tweet(yesterday).await?;
+    let toggl_text_for_tweets = get_toggle_text_for_tweet(yesterday).await?;
+    for tweet in toggl_text_for_tweets {
+        info!("{tweet} {}", tweet.len());
+        create_tweet(tweet.as_str()).await;
+    }
+    for tweet in todoist_text_for_tweets {
+        info!("{tweet} {}", tweet.len());
+        create_tweet(tweet.as_str()).await;
+    }
     Ok(())
 }
